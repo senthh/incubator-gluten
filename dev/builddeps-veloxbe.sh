a455695eda07424085af0f3427fa9ab62849681b
senthh <senthil.kumar@acceldata.io>
2025-02-06 19:07:40 +0530

    ODP-3126 - Added spark 3.5.1 profile

--- dev/builddeps-veloxbe.sh
+++ dev/builddeps-veloxbe.sh
@@ -168,7 +168,7 @@ fi
 
 if [ "$SPARK_VERSION" = "3.2" ] || [ "$SPARK_VERSION" = "3.3" ] \
   || [ "$SPARK_VERSION" = "3.4" ] || [ "$SPARK_VERSION" = "3.5" ] \
-  || [ "$SPARK_VERSION" = "ALL" ]; then
+  || [ "$SPARK_VERSION" = "3.5.1" ]|| [ "$SPARK_VERSION" = "ALL" ]; then
   echo "Building for Spark $SPARK_VERSION"
 else
   echo "Invalid Spark version: $SPARK_VERSION"
